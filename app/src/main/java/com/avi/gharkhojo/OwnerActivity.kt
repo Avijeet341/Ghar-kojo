cce7d0a110ef7c7b3a0ddd74913772ab5470539a
Avijeet#21 <80438115+Avijeet341@user.noreply.github.com>
2024-08-05 18:00:26 +0530

    OwnerActivity

--- app/src/main/java/com/avi/gharkhojo/OwnerActivity.kt
+++ app/src/main/java/com/avi/gharkhojo/OwnerActivity.kt
@@ -1,10 +1,14 @@
 package com.avi.gharkhojo
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
@@ -15,8 +19,16 @@ class OwnerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_owner)
+
+        // Set status bar color to black
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, android.R.color.black)
+
+        // Optional: If you want white icons on the black status bar
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
