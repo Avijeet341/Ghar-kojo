4a10cdc5ff71a7a62a9b873c687705e7d2bb6ae4
Avijeet#21 <80438115+Avijeet341@user.noreply.github.com>
2025-01-30 13:24:52 +0530

    no change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,8 @@ dependencies {
     implementation ("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
 
-    
+    implementation ("com.facebook.shimmer:shimmer:0.5.0")
+
 
 
 }
