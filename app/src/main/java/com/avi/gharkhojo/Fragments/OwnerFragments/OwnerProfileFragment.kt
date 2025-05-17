ed04750c794b3e1567d35f5d48bc005c3d5fc7a1
Avijeet#21 <80438115+Avijeet341@user.noreply.github.com>
2025-05-17 16:56:03 +0530

    owner profile layout and fragment

--- app/src/main/java/com/avi/gharkhojo/Fragments/OwnerFragments/OwnerProfileFragment.kt
+++ app/src/main/java/com/avi/gharkhojo/Fragments/OwnerFragments/OwnerProfileFragment.kt
@@ -6,55 +6,70 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.avi.gharkhojo.R
+import com.avi.gharkhojo.databinding.FragmentOwnerProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OwnerProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OwnerProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentOwnerProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_owner_profile, container, false)
+    ): View {
+
+        _binding = FragmentOwnerProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        setupProfileInfo()
+        setupButtons()
+    }
+
+    private fun setupProfileInfo() {
+        // Profile stats
+        binding.postsCount.text = "23"
+        binding.followersCount.text = "79"
+        binding.TenetsCount.text = "151"
+        binding.usernameDisplay.text = "@username"
+
+        // Contact information
+        binding.textViewEmail.text = "user@example.com"
+        binding.textViewUsername.text = "username"
+        binding.textViewPhone.text = "+91 9876543210"
+
+        // Address information
+        binding.textViewHouseNo.text = "123"
+        binding.textRoadNo.text = "Lane 4"
+        binding.textViewColony.text = "Green Park"
+        binding.textViewArea.text = "South Extension"
+        binding.textViewCity.text = "New Delhi"
+        binding.textViewState.text = "Delhi"
+        binding.textViewPincode.text = "110001"
+    }
+
+    private fun setupButtons() {
+        binding.editProfileButton.setOnClickListener {
+
+        }
+
+        binding.shareProfileButton.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OwnerProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OwnerProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = OwnerProfileFragment()
     }
 }
